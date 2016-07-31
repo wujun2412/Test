492a4a65b120427cc855320da5cf5d460c0bbeb6
wujun <wujun2412@163.com>
2016-07-31 20:36:42 +0800

    测试web工程

---
+++ test/src/main/java/com/wujun/Test.java
@@ -0,0 +1,11 @@
+package com.wujun;
+
+/**
+ * Created by wujun on 16/7/31.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+
+}
